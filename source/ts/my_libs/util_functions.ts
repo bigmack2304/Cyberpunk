4c92adf56140a71a4ff7646860b5432c6756eb14
bigmack2304 <dumon7@yandex.ru>
2024-01-20 00:25:27 +0700

    add hardcode
    - контекстное меню не всегда правильно позиционировалось.
    пришлось захарткодить этот проект.
    Нет смысла в этом случае чтото выдумывать.

--- source/ts/my_libs/util_functions.ts
+++ source/ts/my_libs/util_functions.ts
@@ -1,7 +1,21 @@
 // возвращает позицию курсора из события на ДОМ элементе
 function get_cursor_position(this: Element, evt: MouseEvent) {
     const target = this.getBoundingClientRect();
-    const x = evt.clientX - target.left;
+
+    // пока этот код юзается только для одного элемента.
+    // getBoundingClientRect в моем случае не всегда правильно высчитывает положение left (из за своиства родителя max-width, а другой родитель его центрует) , поэтому вручную его корректирую
+    // в исходя из стилей max-width.
+    // посути это делает этот код не переиспользуемым
+    const offset = () => {
+        let offset = window.innerWidth - 1280;
+        if (offset >= 0) {
+            return Math.floor(offset / 2);
+        }
+
+        return 0;
+    };
+
+    const x = evt.clientX - target.left + offset();
     const y = evt.clientY - target.top;
 
     return {
